1f9e8475e738cd153cb8453a7e0bddd57f31a1e6
Cristian A <zink.krysty@gmail.com>
2022-11-15 19:36:17 +0100

    Refactor MechProgramming out of index.tsx

---
+++ src/components/IconizedInstructionPanel.tsx
@@ -0,0 +1,66 @@
+import {
+    InstructionKey,
+    INSTRUCTION_ICON_MAP,
+    INSTRUCTION_KEYS,
+} from "../constants/constants";
+
+const IconizedInstructionPanel = ({
+    programKeyDown,
+    onPress,
+}: {
+    programKeyDown: { [key: InstructionKey]: boolean };
+    onPress: (key: InstructionKey) => void;
+}) => {
+    let colors: { [key: InstructionKey]: string } = {};
+    INSTRUCTION_KEYS.map((key, key_i) => {
+        const isDown = programKeyDown[key];
+        if (isDown) {
+            colors[key] = "#FFFE71";
+        } else {
+            colors[key] = "#FFFFFFFF";
+        }
+    });
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                margin: "0rem 0 2rem 0",
+                justifyContent: "center",
+            }}
+        >
+            {INSTRUCTION_KEYS.map((key, key_i) => {
+                return (
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            textAlign: "center",
+                            width: "2.5rem",
+                            marginRight: "0.3rem",
+                            padding: "0.3rem",
+                            border: "1px solid #CCCCCC",
+                            borderRadius: "0.8rem",
+                            backgroundColor: colors[key],
+                            transitionDuration: "50ms",
+                            cursor: "pointer",
+                        }}
+                        onClick={() => onPress(key)}
+                    >
+                        <i
+                            className="material-icons"
+                            style={{ fontSize: "1rem" }}
+                        >
+                            {INSTRUCTION_ICON_MAP[key]}
+                        </i>
+                        <p style={{ marginTop: "0.1rem", marginBottom: "0" }}>
+                            {key}
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default IconizedInstructionPanel;
---
+++ src/components/MechProgramming.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import { InstructionKey, INSTRUCTION_KEYS } from "../constants/constants";
+import { reorder } from "../helpers/reorder";
+import Grid from "../types/Grid";
+import MechState from "../types/MechState";
+import { BgStatus } from "../types/UnitState";
+import IconizedInstructionPanel from "./IconizedInstructionPanel";
+import MechInput from "./MechInput";
+
+interface MechProgrammingProps {
+    animationState: string;
+    mechCarries: BgStatus[];
+    mechIndexHighlighted: number;
+    mechStates: MechState[];
+    mechInitPositions: Grid[];
+    programs: string[];
+    onProgramsChange: (programs: string[]) => void;
+    onMechInitPositionsChange: (mechInitPositions: Grid[]) => void;
+    onMechIndexHighlight: (index: number) => void;
+}
+
+const MechProgramming = ({
+    animationState,
+    mechCarries,
+    mechIndexHighlighted,
+    mechInitPositions,
+    mechStates,
+    programs,
+    onProgramsChange,
+    onMechInitPositionsChange,
+    onMechIndexHighlight,
+}: MechProgrammingProps) => {
+    let programKeyDownInit = {};
+    for (const key of INSTRUCTION_KEYS) {
+        programKeyDownInit[key] = false;
+    }
+    const [programKeyDown, setProgramKeyDown] = useState<{ [key: InstructionKey]: boolean }>(programKeyDownInit);
+
+    const numMechs = programs.length;
+
+    const handleDragEnd = ({ destination, source }: DropResult) => {
+        // dropped outside the list
+        if (!destination) return;
+
+        const newPrograms = reorder(programs, source.index, destination.index);
+        const newPositions = reorder(mechInitPositions, source.index, destination.index);
+
+        onProgramsChange(newPrograms);
+        onMechInitPositionsChange(newPositions);
+    };
+
+    const handleKeyMechInputProgram = (event, typ: string) => {
+        // typ takes 'down' or 'up'
+        const key = event.key;
+        if (INSTRUCTION_KEYS.includes(key)) {
+            // console.log(`> key ${typ} mech input program:`, event.key)
+            const bool = typ == "down" ? true : false;
+            setProgramKeyDown((prev) => {
+                let prev_copy = JSON.parse(JSON.stringify(prev));
+                prev_copy[key] = bool;
+                return prev_copy;
+            });
+        }
+    };
+
+    function handleMouseOverMechInput(mech_i: number) {
+        onMechIndexHighlight(mech_i);
+    }
+
+    function handleMouseOutMechInput(mech_i: number) {
+        onMechIndexHighlight(-1);
+    }
+
+    const handleInstructionPanelPress = (key: InstructionKey) => {
+        // No selected mech, return
+
+        // Insert the instruction in the currently selected mech
+        console.log("key pressed", key);
+    };
+
+    function handleMechInitPositionChange(mech_i: number, position: Grid) {
+        let newPositions = JSON.parse(JSON.stringify(mechInitPositions));
+        newPositions[mech_i] = position;
+        onMechInitPositionsChange(newPositions);
+    }
+
+    return (
+        <>
+            <IconizedInstructionPanel programKeyDown={programKeyDown} onPress={handleInstructionPanelPress} />
+
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId="mech-input-list" isDropDisabled={animationState !== "Stop"}>
+                    {(provided) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps}>
+                            {animationState == "Stop"
+                                ? Array.from({ length: numMechs }).map((_, mech_i) => (
+                                      <MechInput
+                                          key={`mech-input-${mech_i}`}
+                                          mechIndex={mech_i}
+                                          position={mechInitPositions[mech_i]}
+                                          program={programs[mech_i]}
+                                          pc={0}
+                                          onPositionChange={(index, position) => {
+                                              handleMechInitPositionChange(index, position);
+                                          }}
+                                          onProgramChange={(index, program) =>
+                                              onProgramsChange(programs.map((p, i) => (i === index ? program : p)))
+                                          }
+                                          disabled={animationState == "Stop" ? false : true}
+                                          handleMouseOver={() => {
+                                              handleMouseOverMechInput(mech_i);
+                                          }}
+                                          handleMouseOut={() => {
+                                              handleMouseOutMechInput(mech_i);
+                                          }}
+                                          handleKeyDown={(event) => {
+                                              handleKeyMechInputProgram(event, "down");
+                                          }}
+                                          handleKeyUp={(event) => {
+                                              handleKeyMechInputProgram(event, "up");
+                                          }}
+                                          unitBgStatus={mechCarries[mech_i]}
+                                      />
+                                  ))
+                                : Array.from({ length: numMechs }).map((_, mech_i) => (
+                                      <MechInput
+                                          key={`mech-input-${mech_i}`}
+                                          mechIndex={mech_i}
+                                          position={mechInitPositions[mech_i]}
+                                          program={programs[mech_i]}
+                                          pc={mechStates[mech_i].pc_next}
+                                          onPositionChange={(index, position) => {}}
+                                          onProgramChange={(index, program) => {}}
+                                          disabled={animationState == "Stop" ? false : true}
+                                          handleMouseOver={() => {
+                                              handleMouseOverMechInput(mech_i);
+                                          }}
+                                          handleMouseOut={() => {
+                                              handleMouseOutMechInput(mech_i);
+                                          }}
+                                          handleKeyDown={() => {}}
+                                          handleKeyUp={() => {}}
+                                          unitBgStatus={mechCarries[mech_i]}
+                                      />
+                                  ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </>
+    );
+};
+
+export default MechProgramming;
