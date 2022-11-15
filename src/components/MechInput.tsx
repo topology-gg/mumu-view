2fc654c88efa8d93a871e3388ab059b4d2b2f1fa
Cristian A <zink.krysty@gmail.com>
2022-11-15 18:26:02 +0100

    Fix deleting last instruction in mech input

--- src/components/MechInput.tsx
+++ src/components/MechInput.tsx
@@ -40,7 +40,7 @@ const MechInput = ({
 }: MechInputProps) => {
     const { t } = useTranslation();
 
-    const instructions: string[] = program.split(",");
+    const instructions: string[] = program ? program.split(",") : [];
     const programLength = instructions.length;
     const currentInstructionIndex = pc % programLength;
 
@@ -67,12 +67,11 @@ const MechInput = ({
     };
 
     const handleInsertInstruction = (instruction) => {
-        const instructions = program.split(",") as string[];
         if (instructions.length > PROGRAM_SIZE_MAX) {
             setProgramStyle((prev) => INVALID_STYLE);
         } else {
             setProgramStyle((prev) => NORMAL_STYLE);
-            const newProgram = program.concat(`,${instruction}`);
+            const newProgram = [...instructions, instruction].join(",");
             onProgramChange(mechIndex, newProgram);
         }
     };
