aad8218b536a4eeeaf5678d68c08129bfb0cbf4a
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-11-20 13:38:21 +0800

    add stardisc call to action under leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -16,32 +16,28 @@ const Leaderboard = ({ loadSolution }) => {
     const th_style = {paddingBottom: '1rem'}
     return (
         <>
-            <p style={{fontSize:'0.9rem'}}>{t("leaderboard.title")}</p>
-            {/* <table style={{marginBottom:'30px'}}>
-                <thead>
-                    <tr>
-                        <th style={th_style}>{t("leaderboard.rank")}</th>
-                        <th style={th_style}>{t("leaderboard.account")}</th>
-                        <th style={th_style}>{t("leaderboard.delivered")}</th>
-                        <th style={th_style}>{t("leaderboard.static_cost")}</th>
-                        <th style={th_style}>{t("leaderboard.latency")}</th>
-                        <th style={th_style}>{t("leaderboard.dynamic_cost")}</th>
-                        <th style={th_style}>{t("leaderboard.block_number")}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        solutions ? solutions.map(
-                            (solution, index) => {
-                                return <LeaderboardRow key={`leaderboard-row-${index}`} solution={solution} index={index} loadSolution={loadSolution}/>;
-                            }
-                        )
-                        :
-                        <>{'loading...'}</>
-                        // <></>
-                    }
-                </tbody>
-            </table> */}
+            <p style={{
+                fontSize:'0.9rem',
+                marginBottom: '0.5rem',
+            }}>{t("leaderboard.title")}</p>
+
+            <div style={{
+                marginBottom: '1.5rem',
+            }}>
+                {t("tutorial.goalLine3_1")}
+                <a
+                    href="https://stardisc.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="stardisc"
+                    style={{
+                        margin:'0'
+                    }}
+                >
+                    <strong>StarDisc</strong>
+                </a>
+                {t("tutorial.goalLine3_2")}
+            </div>
             {
                 solutions ? (
                     <table style={{marginBottom:'30px'}}>
