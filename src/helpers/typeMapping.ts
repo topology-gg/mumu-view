62b59b056b3e1d7bd392f91400afafcddeb6aac1
Cristian A <zink.krysty@gmail.com>
2022-12-25 10:27:47 +0100

    Refactor atom type to bg status mapping

--- src/helpers/typeMapping.ts
+++ src/helpers/typeMapping.ts
@@ -1,4 +1,6 @@
+import { AtomType } from "../types/AtomState";
 import Operator, { OPERATOR_TYPES, PlacingFormula } from "../types/Operator";
+import { BgStatus } from "../types/UnitState";
 
 export const placingFormulaToOperator = (placingFormula: PlacingFormula): Operator => {
     const typ = OPERATOR_TYPES[placingFormula.type];
@@ -8,4 +10,19 @@ export const placingFormulaToOperator = (placingFormula: PlacingFormula): Operat
         input: grids.slice(0, typ.input_atom_types.length),
         output: grids.slice(typ.input_atom_types.length),
     };
+};
+
+const ATOM_TYPES_TO_FREE_BG_STATUS_MAPPING: Record<AtomType, BgStatus> = {
+    [AtomType.VANILLA]: BgStatus.ATOM_VANILLA_FREE,
+    [AtomType.HAZELNUT]: BgStatus.ATOM_HAZELNUT_FREE,
+    [AtomType.CHOCOLATE]: BgStatus.ATOM_CHOCOLATE_FREE,
+    [AtomType.TRUFFLE]: BgStatus.ATOM_TRUFFLE_FREE,
+    [AtomType.SAFFRON]: BgStatus.ATOM_SAFFRON_FREE,
+    [AtomType.TURTLE]: BgStatus.ATOM_TURTLE_FREE,
+    [AtomType.SANDGLASS]: BgStatus.ATOM_SANDGLASS_FREE,
+    [AtomType.WILTED]: BgStatus.ATOM_WILTED_FREE,
+};
+
+export const atomTypeToFreeBgStatus = (atomType: AtomType): BgStatus => {
+    return ATOM_TYPES_TO_FREE_BG_STATUS_MAPPING[atomType];
 };
